a0a67f5756d4395f52406dfe0a02ad308efab1f7
GnawhDaniel <danielhwang02@gmail.com>
2021-08-03 15:17:06 -0700

    Spacing.

--- server/server.py
+++ server/server.py
@@ -2,7 +2,6 @@ import socket
 import threading
 import time
 
-# Test
 
 class Server:
     def __init__(self):
@@ -10,31 +9,46 @@ class Server:
         self.messages = []
         self.start()
 
-    # Handle Individual connections
     def handle_client(self, conn, addr):
+        """
+        Handles individual connections.
+        :param conn:
+        :param addr:
+        :return:
+        """
+
         print(f"[NEW CONNECTION] {addr} connected.")
         conn.send(f'[Connection] Connected. Player {len(self.client_list)}'.encode('utf-8'))
         connected = True
+
         while connected:
+
             msg_len = conn.recv(HEADER).decode('UTF-8')
+
             if msg_len:
                 msg_len = int(msg_len)
                 msg = conn.recv(msg_len).decode('UTF-8')
+
                 if msg == DISCONNECT_MSG:
                     connected = False
                     self.client_list.remove(conn)
+
                 elif msg == PLAYERCOUNT:
                     conn.send(str(threading.activeCount() - 1).encode('utf-8'))
+
                 elif threading.activeCount() - 1 == 2:
                     for c in self.client_list:
                         if c != conn:
                             c.send(msg.encode('utf-8'))
+
                 print(f"[{addr}] {msg}")
 
         conn.close()
 
-    # Handle new connections
     def start(self):
+        """
+        Initializes server and accepts new connections.
+        """
         print("[STARTING] Server is starting.")
         s.listen(2)
         print(f"[LISTENING] Listening on {SERVER}")
@@ -56,4 +70,8 @@ if __name__ == '__main__':
     PLAYERCOUNT = "!!!COUNT!!!"
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(ADDR)
-    serv = Server()
+    try:
+        serv = Server()
+    except Exception:
+        print('Server is closing.')
+    s.close()
